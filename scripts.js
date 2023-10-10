2e67512a2f8ef5a8fb89ecd9150c624e39ddb852
Ilya Zakharov <2146376@gmail.com>
2023-10-11 01:04:19 +0300

    start creating pages

--- scripts.js
+++ scripts.js
@@ -1,22 +1,22 @@
 const scrollContainer = document.querySelector('.scroll-container');
-            const scrollArrowLeft = document.querySelector('.scroll-arrow-left');
-            const scrollArrowRight = document.querySelector('.scroll-arrow-right');
+const scrollArrowLeft = document.querySelector('.scroll-arrow-left');
+const scrollArrowRight = document.querySelector('.scroll-arrow-right');
         
-            scrollArrowLeft.addEventListener('click', () => {
-              scrollContainer.scrollBy({
-                left: -300,
-                behavior: 'smooth'
-              });
-            });
+scrollArrowLeft.addEventListener('click', () => {
+  scrollContainer.scrollBy({
+  left: -300,
+  behavior: 'smooth'
+  });
+});
         
-            scrollArrowRight.addEventListener('click', () => {
-              scrollContainer.scrollBy({
-                left: 300,
-                behavior: 'smooth'
-              });
-            });
+scrollArrowRight.addEventListener('click', () => {
+  scrollContainer.scrollBy({
+    left: 300,
+    behavior: 'smooth'
+    });
+});
 
-            function toggleItem() {
-                const item = document.getElementById('toggleItem');
-                item.classList.toggle('active');
-            }
+function toggleItem() {
+  const item = document.getElementById('toggleItem');
+  item.classList.toggle('active');
+}
